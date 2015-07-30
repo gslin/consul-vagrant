3e19f6a2606a62f34443a274549521821b71fcb7
Gea-Suan Lin <gslin@gslin.org>
2015-07-31 02:25:40 +0800

    Make download and install unzip at the time.

--- consul.sh
+++ consul.sh
@@ -3,11 +3,13 @@
 work() {
     sed -i -e 's/archive.ubuntu.com/jp.archive.ubuntu.com/' /etc/apt/sources.list
 
+    cd /tmp
+    wget https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip &
+
     apt-get -Vy update
     apt-get -Vy install unzip
 
-    cd /tmp
-    wget https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip
+    wait
     unzip 0.5.2_linux_amd64.zip
     mv consul /usr/bin
 }
