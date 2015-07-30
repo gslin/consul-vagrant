7e6f84c2e21b45c8ed167664fa54ca7c6b089104
Gea-Suan Lin <gslin@gslin.org>
2015-07-30 18:46:59 +0800

    Copy scripts to c{2,3}.sh.

---
+++ c3.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+work() {
+    sed -i -e 's/archive.ubuntu.com/jp.archive.ubuntu.com/' /etc/apt/sources.list
+
+    apt-get update
+    apt-get -y dist-upgrade
+
+    apt-get -y install unzip
+
+    cd /tmp
+    wget https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip
+    unzip 0.5.2_linux_amd64.zip
+}
+
+work > /tmp/boot.log 2>&1 &
