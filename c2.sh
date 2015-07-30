8ee2b68f8c108fb104e20e0c760f97e16dff6f31
Gea-Suan Lin <gslin@gslin.org>
2015-07-30 18:49:06 +0800

    Move binary to /usr/bin.

--- c2.sh
+++ c2.sh
@@ -11,6 +11,7 @@ work() {
     cd /tmp
     wget https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip
     unzip 0.5.2_linux_amd64.zip
+    mv consul /usr/bin
 }
 
 work > /tmp/boot.log 2>&1 &
